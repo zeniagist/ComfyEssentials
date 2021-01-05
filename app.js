403a1777db33e3534391d5060a94e7ea78e96d9c
Zenia Gist <zeniagist@gmail.com>
2021-01-05 04:16:34 +0000

    set js variables

---
+++ app.js
@@ -0,0 +1,30 @@
+// declare variables
+const cartBtn = $(".cart-btn");
+const closeCartBtn = $(".close-cart");
+const clearCartBtn = $(".clear-cart");
+const cartDOM = $(".cart");
+const cartOverlay = $(".cart-overlay");
+const cartItems = $(".cart-items");
+const cartTotal = $(".cart-total");
+const cartContent = $(".cart-content");
+const productsDOM = $(".products-center");
+// cart
+let cart = [];
+
+// getting the products
+class Products{
+
+}
+// display products
+class UI{
+
+}
+// local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const ui =  new UI();
+  const products = new Products();
+});
